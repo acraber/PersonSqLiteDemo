ee8273209db341faa928f00f716d329fc79d8e74
adam <acraber2@gmail.com>
2020-07-22 00:50:50 -0700

    After Preliminary Delete Record

--- app/src/main/java/adam/illhaveacompany/personalsqlitedemo/DatabaseHandler.kt
+++ app/src/main/java/adam/illhaveacompany/personalsqlitedemo/DatabaseHandler.kt
@@ -3,6 +3,7 @@ package adam.illhaveacompany.personalsqlitedemo
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
+import android.database.DatabaseUtils
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
--- app/src/main/java/adam/illhaveacompany/personalsqlitedemo/MainActivity.kt
+++ app/src/main/java/adam/illhaveacompany/personalsqlitedemo/MainActivity.kt
@@ -1,7 +1,10 @@
 package adam.illhaveacompany.personalsqlitedemo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,6 +17,14 @@ class MainActivity : AppCompatActivity() {
 
         btn_enter.setOnClickListener {
             addRecord()
+        }
+
+        btn_edit.setOnClickListener{
+
+        }
+
+        btn_delete.setOnClickListener{
+            deleteRecordChosen()
         }
     }
 
@@ -32,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                 et_email.text?.clear()
             }
         }else{
-            Toast.makeText(applicationContext,"Record save attemped (with nothing in the database yet)", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext,"No data inserted", Toast.LENGTH_LONG).show()
         }
     }
 
@@ -40,6 +51,43 @@ class MainActivity : AppCompatActivity() {
        val databaseHandler : DatabaseHandler = DatabaseHandler(this)
         return databaseHandler.viewEmployee()
     }//1
+
+    //used if I don't need to pick the record??
+    fun deleteRecordChosenSimply() {
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        databaseHandler.deleteEmployee(EmpModelClass(3, "", ""))
+    }
+
+    //needs lots of work
+    fun deleteRecordChosen() {
+        val builder = AlertDialog.Builder(this)
+
+
+
+        //I can do val builder1 = builder.setTitle("Delete Record") -- could use this val to add something new to SQL through it.
+        builder.setTitle("Delete Record")
+        builder.setMessage("Are you sure you want to delete it?")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        builder.setPositiveButton("Yes") { dialogInterface, which ->
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+            //the 1 is where I choose which record I delete - can probably change to fun deleteRecordAlertDialog(empModelClass:EmpModelClass) {
+            //and give tell it which one to delete in the argument -- THIS IS WHERE I CHANGE WHICH ONE I WANT TO DELETE
+            val status = databaseHandler.deleteEmployee(EmpModelClass(1, "", ""))
+
+            //I don't need the if statement right now but if I want to add the functionality to choose certain
+            //records I'll need it.
+            if(status > -1){
+                Toast.makeText(applicationContext,"Attempted record deletion", Toast.LENGTH_LONG).show()
+            }
+            dialogInterface.dismiss()
+        }
+
+        //builder seems SUPER powerful
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
 
 
 
